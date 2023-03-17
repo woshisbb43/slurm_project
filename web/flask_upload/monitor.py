f5b176488bf33b08e6f42ff57bd0d36ef8dfd060
ubuntu <ubuntu@localhost.localdomain>
2023-03-17 15:03:40 +0800

    update monitor metrics to show

--- web/flask_upload/monitor.py
+++ web/flask_upload/monitor.py
@@ -6,12 +6,13 @@ def get_metrics():
     metrics = requests.get("http://localhost:8080/metrics").content.decode('utf-8')
 
     metric_data = {}
+    bad_words = ['slurm_nodes_idle', 'slurm_nodes_alloc', 'slurm_partition_cpus_idle', 'slurm_partition_cpus_total', 'slurm_queue_pending', 'slurm_queue_running', 'job', 'slurm_cpus']
 
     # Iterate over metric families
     for family in text_string_to_metric_families(metrics):
         # Extract metric name and help text
         name = family.name
-        if 'go' not in name:
+        if any([x in name for x in bad_words]):
             help_text = family.documentation
 
             # Initialize dictionary for this metric family
@@ -25,7 +26,7 @@ def get_metrics():
                 sample_value = sample.value
 
                 # Add sample data to metric family dictionary
-                metric_family_data["samples"].append({"name": sample_name, "labels": sample_labels, "value": sample_value})
+                metric_family_data["samples"].append({"name": sample_name.replace("_", " "), "labels": sample_labels, "value": sample_value})
 
             # Add metric family dictionary to overall metric data dictionary
             metric_data[name] = metric_family_data
